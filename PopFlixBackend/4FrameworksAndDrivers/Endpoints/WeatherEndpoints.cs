679a9689be79c507b6c7abf7fca7cd66dac1ad55
Jepasej <146959451+Jepasej@users.noreply.github.com>
2025-11-09 16:41:29 +0100

    added weatherendpoints

--- PopFlixBackend/4FrameworksAndDrivers/Endpoints/WeatherEndpoints.cs
+++ PopFlixBackend/4FrameworksAndDrivers/Endpoints/WeatherEndpoints.cs
@@ -1,6 +1,43 @@
-﻿namespace PopFlixBackend._4FrameworksAndDrivers.Endpoints
+﻿using System.Runtime.CompilerServices;
+
+namespace PopFlixBackend._4FrameworksAndDrivers.Endpoints
 {
-    public class WeatherEndpoints
+    public static class WeatherEndpoints 
     {
+        public static IEndpointRouteBuilder MapWeatherEndpoints(this IEndpointRouteBuilder b)
+        {
+            string[] summaries = new[]
+                {
+                    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+                };
+            
+            b.MapGet("weatherforecast", () =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = summaries[Random.Shared.Next(summaries.Length)]
+                    })
+                    .ToArray();
+                return forecast;
+            })
+            .RequireAuthorization();
+
+            b.MapGet("weatherforecast/{index}", (int index) =>
+            {
+                var forecast = new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = summaries[index]
+                };
+
+                return Results.Ok(forecast);
+            });
+
+            return b;
+        }
     }
 }
